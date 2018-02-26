2d8889dd449a41e2fb66ca91f438814adf7bab7d
Mattia Campana <campana.mattia@gmail.com>
2018-02-26 22:17:12 +0100

    .

--- facebook_crawler.py
+++ facebook_crawler.py
@@ -1,6 +1,6 @@
 from facepy import GraphAPI
 
-from model.fbmodel import Post
+from model.fbmodel import Post, Page, WatchedVideo, Music, Book
 from utils.commons import delete_keys_from_dict
 
 
@@ -25,4 +25,58 @@ class FacebookCrawler:
                 post = Post(delete_keys_from_dict(p, ["paging"]))
                 print(post.__dict__)
 
+    def get_liked_pages(self):
+
+        iterator = self.graph.get("me/likes", page=True)
+
+        for data in iterator:
+            for p in data["data"]:
+                page = Page(p)
+                print(page.__dict__)
+
+    def get_watched_videos(self):
+
+        iterator = self.graph.get("me/video.watches", page=True)
+
+        for data in iterator:
+            for p in data["data"]:
+
+                if "data" in p:
+
+                    field = None
+
+                    if "tv_show" in p["data"]:
+                        field = "tv_show"
+                    elif "movie" in p["data"]:
+                        field = "movie"
+                    elif "episode" in p["data"]:
+                        field = "episode"
+                    elif "tv_episode" in p["data"]:
+                        field = "tv_episode"
+                    elif "video" in p["data"]:
+                        field = "video"
+
+                    video = WatchedVideo(p["data"][field])
+                    video.type = field
+                    print(video.__dict__)
+
+    def get_listened_musics(self):
+
+        iterator = self.graph.get("me/music.listens", page=True)
+
+        for data in iterator:
+            for p in data["data"]:
+                if "data" in p and "song" in p["data"]:
+                    music = Music(p["data"]["song"])
+                    print(music.__dict__)
+
+    def get_read_books(self):
+
+        iterator = self.graph.get("me/books.reads", page=True)
+
+        for data in iterator:
+            for p in data["data"]:
+                if "data" in p and "book" in p["data"]:
+                    book = Book(p["data"]["book"])
+                    print(book.__dict__)
 
--- model/fbmodel.py
+++ model/fbmodel.py
@@ -1,4 +1,8 @@
 POST_FIELDS = ("id", "created_time", "link", "message", "message_tags", "type", "object_id", "likes", "comments")
+PAGE_FIELDS = ("id", "name", "created_time")
+BOOK_FIELDS = ("id", "title", "url")
+WATCHED_VIDEO_FIELDS = ("id", "title", "type", "url")
+MUSIC_FIELDS = ("id", "title", "url")
 
 
 class Post:
@@ -6,5 +10,41 @@ class Post:
     def __init__(self, d):
 
         for k in POST_FIELDS:
+            if k in d:
+                self.__dict__[k] = d[k]
+
+
+class Page:
+
+    def __init__(self, d):
+
+        for k in PAGE_FIELDS:
+            if k in d:
+                self.__dict__[k] = d[k]
+
+
+class Book:
+
+    def __init__(self, d):
+
+        for k in BOOK_FIELDS:
+            if k in d:
+                self.__dict__[k] = d[k]
+
+
+class WatchedVideo:
+
+    def __init__(self, d):
+
+        for k in WATCHED_VIDEO_FIELDS:
+            if k in d:
+                self.__dict__[k] = d[k]
+
+
+class Music:
+
+    def __init__(self, d):
+
+        for k in MUSIC_FIELDS:
             if k in d:
                 self.__dict__[k] = d[k]
